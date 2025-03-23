fcc64b97d6de496019cd46c2e4dd2ed9cd3b4ae1
kauansmotaa <kauansmota.xbox@gmail.com>
2025-03-23 15:53:21 -0300

    kkk

--- java.js
+++ java.js
@@ -7,8 +7,11 @@ function appendToDisplay(value) {
 }
 
 function calculate() {
+    let expression = document.getElementById('display').value;
+    expression = expression.replace(/\^/g, '**');
+    expression = expression.replace(/sqrt\(/g, 'Math.sqrt(');
     try {
-        document.getElementById('display').value = eval(document.getElementById('display').value);
+        document.getElementById('display').value = eval(expression);
     } catch {
         document.getElementById('display').value = 'Erro';
     }
