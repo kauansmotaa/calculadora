22dbfd2471c6fac37a8c20d948859a8cb1a9ee17
kauansmotaa <kauansmota.xbox@gmail.com>
2025-03-23 15:56:26 -0300

    jj

--- java.js
+++ java.js
@@ -2,14 +2,19 @@ function clearDisplay() {
     document.getElementById('display').value = '';
 }
 
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
 
 function calculate() {
     let expression = document.getElementById('display').value;
-    expression = expression.replace(/\^/g, '**');
-    expression = expression.replace(/sqrt\(/g, 'Math.sqrt(');
+    expression = expression.replace(/%/g, '/100'); // Transforma "%" em "/100"
+
     try {
         document.getElementById('display').value = eval(expression);
     } catch {
